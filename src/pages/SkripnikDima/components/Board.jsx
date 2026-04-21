74e544d11242e8c8f8f6ad836567dbf9dd9551d3
Dima Skripnik <85933443+RiderTr@users.noreply.github.com>
2026-04-21 16:25:44 +0300

    Add Board component for rendering game grid

---
+++ src/pages/SkripnikDima/components/Board.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Cell from './Cell';
+import styles from './Board.module.css';
+
+const Board = ({ field, onCellClick, onCellContext }) => {
+  return (
+    <div className={styles.boardContainer}>
+      <div 
+        className={styles.board} 
+        style={{ 
+          gridTemplateColumns: `repeat(${field[0].length}, 24px)`,
+          gridTemplateRows: `repeat(${field.length}, 24px)` 
+        }}
+      >
+        {field.map((row, rIdx) => 
+          row.map((cell, cIdx) => (
+            <Cell 
+              key={`${rIdx}-${cIdx}`}
+              data={cell}
+              onClick={() => onCellClick(rIdx, cIdx)}
+              onContextMenu={(e) => {
+                e.preventDefault();
+                onCellContext(rIdx, cIdx);
+              }}
+            />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Board;
