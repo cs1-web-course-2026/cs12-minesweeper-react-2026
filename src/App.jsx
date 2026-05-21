c892fd6cb9634154773f1d133aa159d6bc0ab049
dissolveddd <vladbatechko.qwe@gmail.com>
2026-05-21 14:33:51 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import BarsukovRodion from './pages/BarsukovRodion'
 import SkripnikDima from './pages/SkripnikDima'
 import KoryaginNazar from './pages/KoryaginNazar'
 import KhalinIhor from './pages/KhalinIhor'
+import MinesweeperVlad from './pages/BatechkoVlad';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
         <Route path="skripnik-dima" element={<SkripnikDima />} />
         <Route path="koryagin-nazar" element={<KoryaginNazar />} />
         <Route path="khalin-ihor" element={<KhalinIhor />} />
+        <Route path="/batechko-vlad" element={<MinesweeperVlad />} />
       </Route>
     </Routes>
   )
